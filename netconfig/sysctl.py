a9c6150b749980a6e1ef432e8246e2abc3bbd3c5
charles <charles@ccxtechnologies.com>
2020-07-20 09:34:14 -0400

    convert sysctl into functions from coroutines

--- netconfig/sysctl.py
+++ netconfig/sysctl.py
@@ -1,11 +1,10 @@
 # Copyright: 2020, CCX Technologies
 
-import aiofiles
 
 
-async def sysctl_read(key):
+def sysctl_read(key):
     try:
-        async with aiofiles.open(
+        with open(
                 f'/proc/sys/{key.replace(".", "/")}', mode='r'
         ) as fi:
             return (await fi.read()).strip()
@@ -13,8 +12,8 @@ async def sysctl_read(key):
         return None
 
 
-async def sysctl_write(key, value):
-    async with aiofiles.open(
+def sysctl_write(key, value):
+    with open(
             f'/proc/sys/{key.replace(".", "/")}', mode='w'
     ) as fo:
         await fo.write(value)
