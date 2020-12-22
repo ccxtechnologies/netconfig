6b0fe55c85fda3ddf22af819120f7854244e12eb
charles <charles@ccxtechnologies.com>
2020-12-22 09:50:21 -0500

    switch to one socket per instance

--- netconfig/aiproute.py
+++ netconfig/aiproute.py
@@ -13,131 +13,119 @@ class AIPRoute():
     def __init__(self, loop=None, executor=None):
         self.loop = asyncio.get_event_loop() if loop is None else loop
         self.executor = executor
+        self.ipr = IPRoute()
+
+    def close(self):
+        if not self.ipr.closed:
+            self.ipr.close()
 
     def _get_id(self, device_name: str) -> None:
-        with IPRoute() as ipr:
-            device_ids = ipr.link_lookup(ifname=device_name)
+        device_ids = self.ipr.link_lookup(ifname=device_name)
 
-            try:
-                return device_ids[0]
-            except IndexError:
-                return 0
+        try:
+            return device_ids[0]
+        except IndexError:
+            return 0
 
     def _get_name(self, device_id: int) -> str:
-        with IPRoute() as ipr:
-            links = ipr.get_links(device_id)
-            try:
-                return links[0].get_attr('IFLA_IFNAME')
-            except (IndexError, KeyError):
-                return None
+        links = self.ipr.get_links(device_id)
+        try:
+            return links[0].get_attr('IFLA_IFNAME')
+        except (IndexError, KeyError):
+            return None
 
     def _delete_device(self, device_name: str) -> None:
-        with IPRoute() as ipr:
-            try:
-                ipr.link('del', ifname=device_name)
-            except NetlinkError as exc:
-                if exc.code == 19:
-                    # if it doesn't exist that's okay
-                    pass
-                else:
-                    raise
+        try:
+            self.ipr.link('del', ifname=device_name)
+        except NetlinkError as exc:
+            if exc.code == 19:
+                # if it doesn't exist that's okay
+                pass
+            else:
+                raise
 
     def _set_master(self, device_id: int, master_id: int) -> None:
-        with IPRoute() as ipr:
-            try:
-                ipr.link('set', index=device_id, master=master_id)
+        try:
+            self.ipr.link('set', index=device_id, master=master_id)
 
-            except (OSError, NetlinkError):
-                raise RuntimeError(
-                        f"Failed to add {device_id} to bridge {master_id}"
-                )
+        except (OSError, NetlinkError):
+            raise RuntimeError(
+                    f"Failed to add {device_id} to bridge {master_id}"
+            )
 
     def _add_device(
             self, device_name: str, device_type: str, **kwargs
     ) -> None:
-        with IPRoute() as ipr:
-            ipr.link('add', ifname=device_name, kind=device_type, **kwargs)
+        self.ipr.link('add', ifname=device_name, kind=device_type, **kwargs)
 
     def _set_address(self, device_id: int, address: netaddr.IPNetwork) -> None:
-        with IPRoute() as ipr:
-            ipr.flush_addr(index=device_id)
+        self.ipr.flush_addr(index=device_id)
 
-            if bool(address.ip) and bool(address.prefixlen):
-                ipr.addr(
-                        'add',
-                        index=device_id,
-                        address=str(address.ip),
-                        mask=address.prefixlen
-                )
+        if bool(address.ip) and bool(address.prefixlen):
+            self.ipr.addr(
+                    'add',
+                    index=device_id,
+                    address=str(address.ip),
+                    mask=address.prefixlen
+            )
 
     def _set_mtu(self, device_id: int, mtu: int) -> None:
-        with IPRoute() as ipr:
-            ipr.link('set', index=device_id, mut=mtu)
+        self.ipr.link('set', index=device_id, mut=mtu)
 
     def _set_up(self, device_id: int, state: bool) -> None:
-        with IPRoute() as ipr:
-            ipr.link('set', index=device_id, state='up' if state else 'down')
+        self.ipr.link('set', index=device_id, state='up' if state else 'down')
 
     def _flush_rules(self, **kwargs) -> None:
-        with IPRoute() as ipr:
-            try:
-                ipr.flush_rules(**kwargs)
-            except (OSError, NetlinkError):
-                pass
+        try:
+            self.ipr.flush_rules(**kwargs)
+        except (OSError, NetlinkError):
+            pass
 
     def _delete_rule(self, **kwargs) -> None:
-        with IPRoute() as ipr:
-            try:
-                ipr.rule('delete', **kwargs)
-            except NetlinkError:
-                pass
+        try:
+            self.ipr.rule('delete', **kwargs)
+        except NetlinkError:
+            pass
 
     def _add_rule(self, **kwargs) -> None:
-        with IPRoute() as ipr:
-            try:
-                ipr.rule('add', **kwargs)
-            except NetlinkError as exc:
-                if exc.code == 17:
-                    pass
-                else:
-                    raise RuntimeError(f"Failed to add rule {kwargs}: {exc}")
+        try:
+            self.ipr.rule('add', **kwargs)
+        except NetlinkError as exc:
+            if exc.code == 17:
+                pass
+            else:
+                raise RuntimeError(f"Failed to add rule {kwargs}: {exc}")
 
     def _flush_routes(self, **kwargs) -> None:
-        with IPRoute() as ipr:
-            try:
-                ipr.flush_routes(**kwargs)
-            except (OSError, NetlinkError):
-                pass
+        try:
+            self.ipr.flush_routes(**kwargs)
+        except (OSError, NetlinkError):
+            pass
 
     def _delete_route(self, **kwargs) -> None:
-        with IPRoute() as ipr:
-            try:
-                ipr.route('delete', **kwargs)
-            except NetlinkError:
-                pass
+        try:
+            self.ipr.route('delete', **kwargs)
+        except NetlinkError:
+            pass
 
     def _add_route(self, **kwargs) -> None:
-        with IPRoute() as ipr:
-            try:
-                ipr.route('add', **kwargs)
-            except NetlinkError as exc:
-                if exc.code == 17:
-                    time.sleep(0.250)
-                    try:
-                        ipr.route('add', **kwargs)
-                    except NetlinkError as exc:
-                        raise RuntimeError(
-                                f"Failed to add route {kwargs}: {exc}"
-                        )
-                else:
+        try:
+            self.ipr.route('add', **kwargs)
+        except NetlinkError as exc:
+            if exc.code == 17:
+                time.sleep(0.250)
+                try:
+                    self.ipr.route('add', **kwargs)
+                except NetlinkError as exc:
                     raise RuntimeError(f"Failed to add route {kwargs}: {exc}")
+            else:
+                raise RuntimeError(f"Failed to add route {kwargs}: {exc}")
 
     def _run_routine(self, routine):
-        with IPRoute() as ipr:
-            try:
-                routine(ipr)
-            except NetlinkError as exc:
-                raise RuntimeError(f"Netlink error in {routine}: {exc}")
+        try:
+            routine(self.ipr)
+        except NetlinkError as exc:
+            raise RuntimeError(f"Netlink error in {routine}: {exc}")
 
     async def get_id(self, device_name: str) -> int:
         if not device_name:
