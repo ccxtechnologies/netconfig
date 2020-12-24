42f928f0f6a10de402ad316f3dc16bbb2a93d6ff
charles <charles@ccxtechnologies.com>
2020-12-24 10:18:48 -0500

    add a get_up_and_lower_up method to iface

--- netconfig/iface.py
+++ netconfig/iface.py
@@ -305,6 +305,11 @@ class Iface:
         fcntl.ioctl(self.sock, SIOCGIFFLAGS, ifr)
         return bool(ifr.data.ifr_flags & IFF_UP)
 
+    def get_up_and_lower_up(self):
+        ifr = self._ifreq()
+        fcntl.ioctl(self.sock, SIOCGIFFLAGS, ifr)
+        return bool(ifr.data.ifr_flags & (IFF_UP | IFF_LOWER_UP))
+
     def set_up(self, value):
         ifr = self._ifreq()
         fcntl.ioctl(self.sock, SIOCGIFFLAGS, ifr)
