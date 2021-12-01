fc028f246464e43cf5b341bcc9e8d65376aea24d
Ini-Abasi <ini-abasi@ccxtechnologies.com>
2021-12-01 08:25:40 -0500

    resolved some ctest checker errors

--- netconfig/__init__.py
+++ netconfig/__init__.py
@@ -1,7 +1,5 @@
 # == Copyright: 2017-2020, CCX Technologies
 
-# flake8: noqa
-
 from .__version__ import __version__
 
 from .ethtool import EthTool
@@ -11,3 +9,8 @@ from .netlink import monitor_state_change
 from .sysctl import sysctl_read
 from .sysctl import sysctl_write
 from .aiproute import AIPRoute
+
+__all__ = [
+        "__version__", "EthTool", "Iface", "mdio_read_reg",
+        "monitor_state_change", "sysctl_read", "sysctl_write", "AIPRoute"
+]
