a13748fc500a97bb620770bdc28328ecc53c9aa6
charles <charles@ccxtechnologies.com>
2021-12-13 12:34:02 -0500

    convert the wg interface to synchronous

--- netconfig/__init__.py
+++ netconfig/__init__.py
@@ -9,10 +9,10 @@ from .netlink import monitor_state_change
 from .sysctl import sysctl_read
 from .sysctl import sysctl_write
 from .aiproute import AIPRoute
-from .wireguard import WGNetlinkSocket
+from .wgroute import WGRoute
 
 __all__ = [
         "__version__", "EthTool", "Iface", "mdio_read_reg",
         "monitor_state_change", "sysctl_read", "sysctl_write", "AIPRoute",
-        "WGNetlinkSocket"
+        "WGRoute"
 ]
