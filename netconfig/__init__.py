1aed5e1607b00cd475fe477560d0bf9991ca5e28
charles <charles@ccxtechnologies.com>
2022-02-22 15:36:59 -0500

    add start of iwroute config

--- netconfig/__init__.py
+++ netconfig/__init__.py
@@ -10,11 +10,12 @@ from .sysctl import sysctl_read
 from .sysctl import sysctl_write
 from .aiproute import AIPRoute
 from .wgroute import WGRoute
+from .iwroute import IWRoute
 from .route_tables import get_rt_protocol_id
 from .route_tables import get_rt_table_id
 
 __all__ = [
         "__version__", "EthTool", "Iface", "mdio_read_reg",
         "monitor_state_change", "sysctl_read", "sysctl_write", "AIPRoute",
-        "WGRoute", "get_rt_protocol_id", "get_rt_table_id"
+        "WGRoute", "IWRoute", "get_rt_protocol_id", "get_rt_table_id"
 ]
---
+++ netconfig/iwroute.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# Copyright: 2022, CCX Technologies
+
+import asyncio
+from functools import partial
+
+from pr2modules.netlink.nl80211 import nl80211cmd
+from pr2modules.netlink.nl80211 import NL80211_NAMES
+from pr2modules.netlink import NLM_F_ACK
+from pr2modules.netlink import NLM_F_REQUEST
+
+from pyroute2 import IW
+
+
+class IWRoute:
+
+    def __init__(self, loop=None, executor=None):
+        self.loop = asyncio.get_event_loop() if loop is None else loop
+        self.executor = executor
+        self.iw = IW()
+
+    def _set_tx_power_limit(self, phy_id, tx_power_dbm):
+
+        msg = nl80211cmd()
+        msg['cmd'] = NL80211_NAMES['NL80211_CMD_SET_WIPHY']
+        msg['attrs'] = [
+                ['NL80211_ATTR_WIPHY', phy_id],
+                ['NL80211_ATTR_WIPHY_TX_POWER_SETTING', 1],
+        ]
+        msg['attrs'].append(
+                ['NL80211_ATTR_WIPHY_TX_POWER_LEVEL', 100 * tx_power_dbm]
+        )
+
+        self.iw.nlm_request(
+                msg,
+                msg_type=self.iw.prid,
+                msg_flags=NLM_F_REQUEST | NLM_F_ACK
+        )
+
+    async def set_tx_power_limit(self, phy_id: int, tx_power_dbm: int) -> None:
+        if phy_id < 0:
+            return
+
+        await self.loop.run_in_executor(
+                self.executor,
+                partial(self._set_tx_power_limit, phy_id, tx_power_dbm)
+        )
+
+    def close(self):
+        self.iw.close()
