2a81568d632c4f11d37b4999682bbdd68f3e1293
charles <charles@ccxtechnologies.com>
2022-02-23 07:24:41 -0500

    add iw add device routines

--- netconfig/iwroute.py
+++ netconfig/iwroute.py
@@ -8,6 +8,7 @@ from pr2modules.netlink.nl80211 import nl80211cmd
 from pr2modules.netlink.nl80211 import NL80211_NAMES
 from pr2modules.netlink import NLM_F_ACK
 from pr2modules.netlink import NLM_F_REQUEST
+from pyroute2.netlink.exceptions import NetlinkError
 
 from pyroute2 import IW
 
@@ -43,12 +44,82 @@ class IWRoute:
                 for intf in self.iw.get_interface_by_phy(phy_id)
         ]
 
+    def _get_id(self, phy_id, device_name):
+        for _id, _name in [
+                (
+                        intf.get_attr('NL80211_ATTR_IFINDEX'),
+                        intf.get_attr('NL80211_ATTR_IFNAME')
+                ) for intf in self.iw.get_interface_by_phy(phy_id)
+        ]:
+            if _name == device_name:
+                return _id
+
+        return None
+
+    def _add_device(
+            self, phy_id: int, device_name: str, device_type: str
+    ) -> None:
+        try:
+            self.iw.add_interface(
+                    ifname=device_name, iftype=device_type, phy=phy_id
+            )
+        except NetlinkError as exc:
+            if exc.code == 17:
+                raise FileExistsError(
+                        f"Device {device_name} already exists"
+                ) from exc
+            else:
+                raise
+
+        for _ in range(0, 10):
+            _id = self._get_id(phy_id, device_name)
+            if _id:
+                break
+        else:
+            raise RuntimeError(f"Failed to add {device_name}")
+
+        return _id
+
     async def get_phy_device_ids(self, phy_id: int) -> list:
         if phy_id < 0:
             return None
 
         return await self.loop.run_in_executor(
                 self.executor, partial(self._get_phy_device_ids, phy_id)
+        )
+
+    async def get_id(self, phy_id: int, device_name: str) -> int:
+        if phy_id < 0 or not device_name:
+            return None
+
+        return await self.loop.run_in_executor(
+                self.executor, partial(self._get_id, phy_id, device_name)
+        )
+
+    async def add_device(
+            self, phy_id: int, device_name: str, device_type
+    ) -> int:
+        """
+        device_type can be:
+            1. adhoc
+            2. station
+            3. ap
+            4. ap_vlan
+            5. wds
+            6. monitor
+            7. mesh_point
+            8. p2p_client
+            9. p2p_go
+            10. p2p_device
+            11. ocb
+        """
+
+        if not device_name or not device_type:
+            return
+
+        return await self.loop.run_in_executor(
+                self.executor,
+                partial(self._add_device, phy_id, device_name, device_type)
         )
 
     async def delete_device(self, device_id: int) -> None:
