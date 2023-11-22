e233f728d033f433b9e7f7ce0e654b5601093b3d
charles <charles@ccxtechnologies.com>
2023-11-22 09:18:08 -0500

    improve performance of route_tables on sysytems that read from the table files a lot

--- netconfig/route_tables.py
+++ netconfig/route_tables.py
@@ -1,22 +1,35 @@
-# Copyright: 2022, CCX Technologies
+# Copyright: 2022-2023, CCX Technologies
 
-import aiofiles
+rt_files: dict = {}
 
 
-async def _get_rt_value(name: str, rt_file: str) -> int:
-    async with aiofiles.open(f"/etc/iproute2/{rt_file}", "r") as fi:
-        async for line in fi:
-            if line[0] == '#':
-                continue
-            label = line.split(' ', 1)[-1].split('\t')[-1].strip()
-            if label == name:
-                return int(line.split()[0])
-    raise RuntimeError(f"Unable to find {name} in {rt_file}")
+def get_rt_value(name: str, rt_file: str, rt_files_dict: dict) -> int:
+    rt_data = get_rt_file(rt_file, rt_files_dict)
+
+    try:
+        return rt_data[name]
+    except KeyError:
+        raise RuntimeError(f"Unable to find {name} in {rt_file}")
+
+
+def get_rt_file(rt_file: str, rt_files_dict: dict):
+    if not rt_files_dict.setdefault(rt_file, []):
+        with open(f"/etc/iproute2/{rt_file}", "r") as fi:
+            data = fi.read()
+
+        rt_files[rt_file] = {
+                line.split(' ', 1)[-1].split('\t')[-1].strip():
+                int(line.split()[0])
+                for line in data.splitlines()
+                if line and line[0] != '#'
+        }
+
+    return rt_files[rt_file]
 
 
 async def get_rt_table_id(name: str) -> int:
-    return await _get_rt_value(name, 'rt_tables')
+    return get_rt_value(name, 'rt_tables', rt_files)
 
 
 async def get_rt_protocol_id(name: str) -> int:
-    return await _get_rt_value(name, 'rt_protos')
+    return get_rt_value(name, 'rt_protos', rt_files)
